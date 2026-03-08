9f4b267c4bc6e26c5a7fb7e2b4ce9cf85d2b3f9c
Lucas Matias <lucasmatias2005@gmail.com>
2026-03-07 21:43:05 -0300

    ✨ feat: adding routes and handling local storage

--- readbrew-frontend/src/routers/index.js
+++ readbrew-frontend/src/routers/index.js
@@ -1,35 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../views/HomePageView.vue'
 import LoginView from '../views/LoginView.vue'
-import HomePage from '../views/HomePage.vue'
+import RegisterView from '../views/RegisterView.vue'
+import Cafeteria from '../views/CafeteriaView.vue'
 
 const routes = [
-  {
-    path: '/',
-    redirect: '/login' 
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: LoginView
-  },
-
-  {
-    path: '/',
-    redirect: '/HomePage',
-    component: HomePage
-  },
-  {
-   path: '/HomePage',
-    name: 'HomePage',
-    component: HomePage
-    }
-  
-
+  { path: '/', name: 'home', component: HomePage },
+  { path: '/login', name: 'login', component: LoginView, meta: { requiresGuest: true } },
+  { path: '/register', name: 'register', component: RegisterView, meta: { requiresGuest: true } },
+  { path: '/cafeteria', name: 'cafeteria', component: Cafeteria }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+
+router.beforeEach((to, from) => {
+  const isAuthenticated = !!localStorage.getItem('readbrew_token');
+
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!isAuthenticated) return { name: 'login' }; 
+  } 
+  else if (to.matched.some(record => record.meta.requiresGuest)) {
+    if (isAuthenticated) return { name: 'cafeteria' };
+  } 
+  
+  return true; 
 })
 
 export default router
