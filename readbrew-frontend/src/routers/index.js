14e5287150a9d99f907b4ab4a43e5bb77fbb3206
Lucas Matias <lucasmatias2005@gmail.com>
2026-03-18 14:32:09 -0300

    ✨ feat: adding profile route (sketch)

--- readbrew-frontend/src/routers/index.js
+++ readbrew-frontend/src/routers/index.js
@@ -3,14 +3,15 @@ import HomePage from '../views/HomePageView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import Cafeteria from '../views/CafeteriaView.vue'
-import TimelineView from '../views/Timelineview.vue'
-
+import TimelineView from '../views/TimelineView.vue'
+import ProfileView from '../views/ProfileView.vue'
 const routes = [
   { path: '/', name: 'home', component: HomePage },
   { path: '/login', name: 'login', component: LoginView, meta: { requiresGuest: true } },
   { path: '/register', name: 'register', component: RegisterView, meta: { requiresGuest: true } },
   { path: '/cafeteria', name: 'cafeteria', component: Cafeteria },
-  { path: '/timeline', name: 'timeline', component: TimelineView, meta: {requiresGuest: true} }
+  { path: '/timeline', name: 'timeline', component: TimelineView, meta: { requiresAuth: true } },
+  { path: '/profile', name: 'profile', component: ProfileView, meta: { requiresAuth: true } }
 ]
 
 const router = createRouter({
